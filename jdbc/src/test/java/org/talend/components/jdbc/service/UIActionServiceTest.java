51c864ebd723989035f5bcfbfea607a35785f069
pyzhou <pyzhou@talend.com>
2023-03-20 10:25:45 +0800

    copy right

--- jdbc/src/test/java/org/talend/components/jdbc/service/UIActionServiceTest.java
+++ jdbc/src/test/java/org/talend/components/jdbc/service/UIActionServiceTest.java
@@ -1,5 +1,16 @@
+/*
+ * Copyright (C) 2006-2023 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.talend.components.jdbc.service;
-
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,16 +24,18 @@ class UIActionServiceTest {
     @Service
     private UIActionService uiActionService;
 
-    @Test void validateSQLInjection() {
+    @Test
+    void validateSQLInjection() {
 
-        String[] correct = {"TableName","_my_identifier","My$identifier","идентификатор","内清表","3rd_identifier"};
+        String[] correct = { "TableName", "_my_identifier", "My$identifier", "идентификатор", "内清表", "3rd_identifier" };
         for (String name : correct) {
             final ValidationResult validationResult = uiActionService.validateSQLInjection(name);
 
             Assertions.assertTrue("the table name is valid".equals(validationResult.getComment()));
         }
 
-        String[] riskyOnes = {"\"AAA\\\" ; drop table \\\"ABCDE\" ","105 OR 1=1" ,"45 --","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa259"};
+        String[] riskyOnes = { "\"AAA\\\" ; drop table \\\"ABCDE\" ", "105 OR 1=1", "45 --",
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa259" };
         for (String name : riskyOnes) {
             final ValidationResult validationResult = uiActionService.validateSQLInjection(name);
             Assertions.assertFalse("the table name is valid".equals(validationResult.getComment()));
