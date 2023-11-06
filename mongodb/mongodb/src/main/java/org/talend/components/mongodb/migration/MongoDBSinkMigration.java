d8de590c989363327f121d9137f87a9c8629b7a9
vyu-talend <vyu@talend.com>
2023-11-06 16:53:07 +0800

    feat(TDI-45778): fix format violations.

--- mongodb/mongodb/src/main/java/org/talend/components/mongodb/migration/MongoDBSinkMigration.java
+++ mongodb/mongodb/src/main/java/org/talend/components/mongodb/migration/MongoDBSinkMigration.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (C) 2006-2023 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.talend.components.mongodb.migration;
 
 import org.talend.components.mongo.WriteConcern;
@@ -6,12 +18,13 @@ import org.talend.sdk.component.api.component.MigrationHandler;
 import java.util.Map;
 
 public class MongoDBSinkMigration implements MigrationHandler {
+
     @Override
     public Map<String, String> migrate(int incomingVersion, Map<String, String> incomingData) {
-        if(incomingVersion == 1){
+        if (incomingVersion == 1) {
             String isSetWriteConcern = incomingData.get("setWriteConcern");
             String writeConcern = incomingData.get("writeConcern");
-            if("true".equals(isSetWriteConcern) && "REPLICA_ACKNOWLEDGED".equals(writeConcern)){
+            if ("true".equals(isSetWriteConcern) && "REPLICA_ACKNOWLEDGED".equals(writeConcern)) {
                 incomingData.put("writeConcern", String.valueOf(WriteConcern.W2));
             }
         }
